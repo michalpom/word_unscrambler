5dd825f1bc933bf1bc234bda985239b15df83f24
michalpom <michalpom@gmail.com>
2019-04-16 23:03:57 +0200

    WordMatcher class finished

--- word_unscrambler/Workers/WordMatcher.cs
+++ word_unscrambler/Workers/WordMatcher.cs
@@ -9,9 +9,50 @@ namespace word_unscrambler.Workers
 {
     class WordMatcher
     {
-        internal List<MatchedWord> Match(string[] scrambleWords, string[] wordList)
+        public List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
         {
-            throw new NotImplementedException();
+            var matchedWords = new List<MatchedWord>();
+
+            foreach (var scrambledWord in scrambledWords)
+            {
+                foreach (var word in wordList)
+                {
+                    if (scrambledWord.Equals(word, StringComparison.OrdinalIgnoreCase))
+                    {
+                        matchedWords.Add(BuildMatchedWord(scrambledWord,word));
+                    }
+                    else
+                    {
+                        var scrambledWordArray = scrambledWord.ToCharArray();
+                        var wordArray = word.ToCharArray();
+
+                        Array.Sort(scrambledWordArray);
+                        Array.Sort(wordArray);
+
+                        var sortedScrambledWord =new string(scrambledWordArray);
+                        var sortedWord = new string(wordArray);
+                        //if sorted are equal -> u can make one word from another
+                        if (sortedScrambledWord.Equals(sortedWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            matchedWords.Add(BuildMatchedWord(scrambledWord, word));
+                        }
+
+                    }
+                }
+            }
+
+            return matchedWords;
+        }
+
+        private MatchedWord BuildMatchedWord(string scrambleWord, string word)
+        {
+            MatchedWord matchedWord = new MatchedWord
+            {
+                ScrambledWord = scrambleWord,
+                Word = word
+            };
+            return matchedWord;
         }
     }
 }
+
