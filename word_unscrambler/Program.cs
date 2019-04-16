e6404fb9051ece46ffb57c185c2595ad000e09b5
michalpom <michalpom@gmail.com>
2019-04-16 21:33:23 +0200

    Firs commit, basic program logic

--- word_unscrambler/Program.cs
+++ word_unscrambler/Program.cs
@@ -10,6 +10,53 @@ namespace word_unscrambler
     {
         static void Main(string[] args)
         {
+            bool continueWordUnscramble = true;
+            do
+            {
+                Console.WriteLine("Please enter the option: F for File M for Manual");
+                var oprion = Console.ReadLine() ?? string.Empty;
+
+                switch(oprion.ToUpper())
+                {
+                    case "F":
+                        Console.Write("Enter scrambled words file name: ");
+                        ExecuteScrambledWorsInFileScenario();
+                          break;
+                    case "M":
+                        Console.Write("Enter scrambled words manually: ");
+                        ExecuteScrambledWorsInManualEntryScenario();
+                        break;
+                    default:
+                        Console.Write("Option was not recognised.");
+                        break;
+
+                }
+                var continueWordUnscrableDecision = string.Empty;
+                do
+                {
+                    Console.WriteLine("Do you want to continue? Y/N");
+                    continueWordUnscrableDecision = (Console.ReadLine() ?? string.Empty);
+
+                } while (!continueWordUnscrableDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) && !continueWordUnscrableDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+
+                continueWordUnscramble = continueWordUnscrableDecision.Equals("Y", StringComparison.OrdinalIgnoreCase);
+
+            } while (continueWordUnscramble);
+
+
+
+
+        }
+
+        private static void ExecuteScrambledWorsInManualEntryScenario()
+        {
+            
+        }
+
+        private static void ExecuteScrambledWorsInFileScenario()
+        {
+            
         }
     }
 }
